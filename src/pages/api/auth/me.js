e709889b47a6448528d7bd1bdca9bd2e5fd86697
dougaaH <159202651+dougaaH@users.noreply.github.com>
2025-09-20 14:49:21 -0300

    correção de erros

--- src/pages/api/auth/me.js
+++ src/pages/api/auth/me.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import prisma from "../../../lib/prisma";
+import prisma from "@/lib/prisma";
 
 export default async function handler(req, res) {
   const authHeader = req.headers.authorization;
